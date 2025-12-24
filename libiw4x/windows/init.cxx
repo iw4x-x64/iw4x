f489eed7ea83a15b68e9dbe874259215206b7ee8
William Roy <wroy@proton.me>
2025-12-24 12:19:43 -0500

    Change Windows system DLLs protection

--- libiw4x/windows/init.cxx
+++ libiw4x/windows/init.cxx
@@ -16,7 +16,14 @@ namespace iw4x
     {
       ExitProcessHk = &ExitProcess;
 
-      detour (ExitProcessHk, &exit_process);
+      DWORD o (0);
+      if (VirtualProtect (reinterpret_cast<void*> (ExitProcessHk),
+                          0,
+                          PAGE_EXECUTE_READWRITE,
+                          &o) != 0)
+      {
+        detour (ExitProcessHk, &exit_process);
+      }
     }
   }
 }
